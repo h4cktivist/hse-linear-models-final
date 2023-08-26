cc6165a2663c62d287c28666376b3361972c80a6
Vadim Popov <popovvadim0605@gmail.com>
2023-08-26 18:36:26 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -42,46 +42,46 @@ tab1, = st.tabs([':chart_with_upwards_trend: Анализ'])
 with tab1:
     st.write(df.describe())
 
-    st.image(Image.open('images/distr.png'))
+    st.image(Image.open('app/images/distr.png'))
     st.write('Возраст клиентов банка распределен равномерно, в выборке присутствует каждая из возрастных групп')
 
-    st.image(Image.open('images/income.png'))
+    st.image(Image.open('app/images/income.png'))
     st.write('Персональный доход клиентов варируется от 24 до 250000 рублей Медиана равна 13854 рублей. Присутствуют '
              'выбросы')
 
-    st.image(Image.open('images/gender-tar.png'))
+    st.image(Image.open('app/images/gender-tar.png'))
     st.write('В целом мужчины и женщины почти одинаково склонны давать положительный ответ на предложения банка, '
              'однако женщины делают это немного чаще, чем мужчины')
 
-    st.image(Image.open('images/socw-tar.png'))
+    st.image(Image.open('app/images/socw-tar.png'))
     st.write('Люди, имеющие работу, дают положительный ответ намного чаще, чем безработные')
 
-    st.image(Image.open('images/socp-tar.png'))
+    st.image(Image.open('app/images/socp-tar.png'))
     st.write('Подобная картина наблюдается и среди работающих людей и ушедших на пенсию. Пенсионеры с меньшей '
              'вероятностью отвечают согласием на предложения банка, в отличие он ещё работающих людей')
 
-    st.image(Image.open('images/flpr-tar.png'))
+    st.image(Image.open('app/images/flpr-tar.png'))
     st.write('В то время наличие в собственности квартиры почти не влияет на ответ клиента. Согласиться на '
              'предложение банка почти одинаково готовы как имеющие в собственности недвижимость, так и не имеющие')
 
-    st.image(Image.open('images/child-tar.png'))
+    st.image(Image.open('app/images/child-tar.png'))
     st.write('Говоря о наличии у клиентов детей, можно заметить, что клиенты, имеющие от 1 до 6 детей реагирют '
              'положительно с большей вероятностью, в отличие от клиентов, имеющих 7-8 детей, которые вовсе не дали '
              'положительного ответа. Однако большинство положительных ответов поступало от клинетов, имеющих 10 детей')
 
-    st.image(Image.open('images/deps-tar.png'))
+    st.image(Image.open('app/images/deps-tar.png'))
     st.write('С количеством иждивенцев наблюдается немного иная картина. Вероятность положительного отклика линейно '
              'растет, начиная от 0 до 4 иждивенцев. Большинство положительных ответов дали клиенты с 5 и 6 иждивенцами')
 
-    st.image(Image.open('images/auto-tar.png'))
+    st.image(Image.open('app/images/auto-tar.png'))
     st.write('Не имеющие автомобиля люди или имеющие всего один дают больше положительных откликов')
 
-    st.image(Image.open('images/tloan-tar.png'))
-    st.image(Image.open('images/cloan-tar.png'))
+    st.image(Image.open('app/images/tloan-tar.png'))
+    st.image(Image.open('app/images/cloan-tar.png'))
     st.write('Картина с наличием кредитов и количеством закрытых кредитов в целом схожа. Наибольшее количество '
              'положительных отзывов поступало от клиентов, имеющих 8 кредитов в общем или 8 закрытых кредитов')
 
-    st.image(Image.open('images/matrix.png'))
+    st.image(Image.open('app/images/matrix.png'))
     st.write('Была так же построена матрица корреляций, между различными признаками в выборке, из которой можно '
              'сделать следующие выводы: ')
     st.write('''
