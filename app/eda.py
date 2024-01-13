962ea9983a70387ac66c2293f28029cae3d6dc72
h4cktivist <popovvadim0605@gmail.com>
2024-01-13 20:49:19 +0500

    refactor: .gitignore add & dataset paths fix

--- app/eda.py
+++ app/eda.py
@@ -4,22 +4,22 @@ import pandas as pd
 
 def preprocess_data() -> pd.DataFrame:
     df_clients = pd.read_csv(
-        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_clients.csv')
+        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_clients.csv')
     df_close_loan = pd.read_csv(
-        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_close_loan.csv')
-    df_job = pd.read_csv('https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_job.csv')
+        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_close_loan.csv')
+    df_job = pd.read_csv('https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_job.csv')
     df_last_credit = pd.read_csv(
-        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_last_credit.csv')
+        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_last_credit.csv')
     df_loan = pd.read_csv(
-        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_loan.csv')
+        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_loan.csv')
     df_pens = pd.read_csv(
-        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_pens.csv')
+        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_pens.csv')
     df_salary = pd.read_csv(
-        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_salary.csv')
+        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_salary.csv')
     df_target = pd.read_csv(
-        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_target.csv')
+        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_target.csv')
     df_work = pd.read_csv(
-        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/D_work.csv')
+        'https://raw.githubusercontent.com/aiedu-courses/stepik_linear_models/main/datasets/clients/D_work.csv')
 
     df_clients = df_clients.rename(columns={'ID': 'ID_CLIENT'})
     df = pd.merge(df_target, df_clients, on='ID_CLIENT', how='left')
